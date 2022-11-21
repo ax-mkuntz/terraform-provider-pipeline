8bdd4ac8f3e113bd328f16bcecafc8ea10d7de90
Matthew Kuntz <matthew.kuntz@automox.com>
2022-11-21 12:44:35 -0500

    implement is_sensitive

--- pkg/pipeline/resource_pipeline_project_integration.go
+++ pkg/pipeline/resource_pipeline_project_integration.go
@@ -30,8 +30,13 @@ type ProjectIntegration struct {
 }
 
 type FormJSONValues struct {
-	Label string `json:"label"`
-	Value string `json:"value"`
+	Label     string `json:"label"`
+	Value     string `json:"value"`
+	Sensitive bool
+}
+
+func (f FormJSONValues) Id() string {
+	return f.Label
 }
 
 type ProjectJSON struct {
@@ -40,7 +45,6 @@ type ProjectJSON struct {
 }
 
 const projectIntegrationsUrl = "pipelines/api/v1/projectintegrations"
-const redactedSecretValue = "********"
 
 func pipelineProjectIntegrationResource() *schema.Resource {
 
@@ -92,6 +96,11 @@ func pipelineProjectIntegrationResource() *schema.Resource {
 						Type:        schema.TypeString,
 						Required:    true,
 						Description: "Value of the input property.",
+					},
+					"is_sensitive": {
+						Type:        schema.TypeBool,
+						Optional:    true,
+						Description: "Is the underlying Value sensitive or not",
 					},
 				},
 			},
@@ -256,22 +265,15 @@ func pipelineProjectIntegrationResource() *schema.Resource {
 	}
 }
 
-func lookupFormJSONValue(values []FormJSONValues, key string) FormJSONValues {
-	for _, value := range values {
-		if value.Label == key {
-			return value
-		}
-	}
-	return FormJSONValues{}
-}
 func unpackFormJSONValues(d *util.ResourceData, key string) []FormJSONValues {
 	var formJSONValues []FormJSONValues
 	keyValues := d.Get(key).([]interface{})
 	for _, keyValue := range keyValues {
 		idx := keyValue.(map[string]interface{})
 		formJSONValue := FormJSONValues{
-			Label: idx["label"].(string),
-			Value: idx["value"].(string),
+			Label:     idx["label"].(string),
+			Value:     idx["value"].(string),
+			Sensitive: idx["is_sensitive"].(bool),
 		}
 		formJSONValues = append(formJSONValues, formJSONValue)
 	}
@@ -284,23 +286,20 @@ func packFormJSONValues(ctx context.Context, d *schema.ResourceData, schemaKey s
 
 	for _, idx := range formJSONValues {
 		keyValue := map[string]interface{}{
-			"label": idx.Label,
-			"value": idx.Value,
+			"label":        idx.Label,
+			"value":        idx.Value,
+			"is_sensitive": idx.Sensitive,
 		}
 
+		lookup := FindConfigurationById(existingValues, idx.Label)
 		// the API will always return the redacted value. Putting this into tf-state will cause a diff every time
 		// as it tries to correct "***" -> "secret_val".
-		if idx.Value == redactedSecretValue {
-			lookup := lookupFormJSONValue(existingValues, idx.Label)
-
+		if lookup != nil && lookup.Sensitive {
 			if lookup.Value != "" {
-				tflog.Debug(ctx, "over-writing key value with existing data value", map[string]interface{}{
-					"label":    idx.Label,
-					"value":    idx.Value,
-					"override": lookup.Value,
-				})
 				keyValue["value"] = lookup.Value
 			}
+			//the incoming `idx` value will always be false, the JFrog API has no concept of this field
+			keyValue["is_sensitive"] = true
 		}
 
 		tflog.Debug(ctx, "packFormJSONValues", keyValue)
--- pkg/pipeline/resource_pipeline_project_integration_test.go
+++ pkg/pipeline/resource_pipeline_project_integration_test.go
@@ -7,28 +7,74 @@ import (
 )
 
 const testFormJsonSchemaKey = "form_json_values"
+const redactedSecretValue = "********"
 
 func TestPackFormJSONValues(t *testing.T) {
 	testCases := map[string]struct {
-		input         map[string]string
-		existingState map[string]string
+		input         []FormJSONValues
+		existingState []FormJSONValues
 		result        map[string]string
 	}{
 		"no_asterisks": {
-			input:         map[string]string{"key_a": "not_a_secret", "key_b": "something_else"},
-			existingState: map[string]string{"key_a": "not_a_secret", "key_b": "something_else"},
-			result:        map[string]string{"key_a": "not_a_secret", "key_b": "something_else"},
+			input: []FormJSONValues{{
+				Label:     "key_a",
+				Value:     "not_a_secret",
+				Sensitive: false,
+			}, {
+				Label:     "key_b",
+				Value:     "something_else",
+				Sensitive: false,
+			},
+			},
+			existingState: []FormJSONValues{{
+				Label:     "key_a",
+				Value:     "not_a_secret",
+				Sensitive: false,
+			}, {
+				Label:     "key_b",
+				Value:     "something_else",
+				Sensitive: false,
+			},
+			},
+			result: map[string]string{"key_a": "not_a_secret", "key_b": "something_else"},
 		},
 		"with_asterisks": {
-			input:         map[string]string{"key_a": redactedSecretValue, "key_b": "something_else"},
-			existingState: map[string]string{"key_a": "super_secret", "key_b": "something_else"},
-			result:        map[string]string{"key_a": "super_secret", "key_b": "something_else"},
+			input: []FormJSONValues{{
+				Label:     "key_a",
+				Value:     redactedSecretValue,
+				Sensitive: true,
+			}, {
+				Label:     "key_b",
+				Value:     "something_else",
+				Sensitive: false,
+			},
+			},
+			existingState: []FormJSONValues{{
+				Label:     "key_a",
+				Value:     "super_secret",
+				Sensitive: true,
+			}, {
+				Label:     "key_b",
+				Value:     "something_else",
+				Sensitive: false,
+			},
+			},
+			result: map[string]string{"key_a": "super_secret", "key_b": "something_else"},
 		},
 		// this technically won't happen, even on resource create; the Input from terraform should have the real value
 		// this is just testing code path safety
 		"with_asterisks_no_state": {
-			input:         map[string]string{"key_a": redactedSecretValue, "key_b": "something_else"},
-			existingState: map[string]string{},
+			input: []FormJSONValues{{
+				Label:     "key_a",
+				Value:     redactedSecretValue,
+				Sensitive: false,
+			}, {
+				Label:     "key_b",
+				Value:     "something_else",
+				Sensitive: false,
+			},
+			},
+			existingState: []FormJSONValues{},
 			result:        map[string]string{"key_a": redactedSecretValue, "key_b": "something_else"},
 		},
 	}
@@ -38,10 +84,11 @@ func TestPackFormJSONValues(t *testing.T) {
 			projectSchemaResource := pipelineProjectIntegrationResource()
 			schemaData := projectSchemaResource.TestResourceData()
 			fJsonState := make([]interface{}, 0)
-			for k, v := range tcase.existingState {
+			for _, idx := range tcase.existingState {
 				fJsonState = append(fJsonState, map[string]interface{}{
-					"label": k,
-					"value": v,
+					"label":        idx.Label,
+					"value":        idx.Value,
+					"is_sensitive": idx.Sensitive,
 				})
 			}
 			err := schemaData.Set(testFormJsonSchemaKey, fJsonState)
@@ -49,12 +96,7 @@ func TestPackFormJSONValues(t *testing.T) {
 				t.Fatalf("error creating test schema %v", err)
 			}
 
-			fJsonInput := make([]FormJSONValues, 0)
-			for k, v := range tcase.input {
-				fJsonInput = append(fJsonInput, FormJSONValues{Label: k, Value: v})
-			}
-
-			errs := packFormJSONValues(context.TODO(), schemaData, testFormJsonSchemaKey, fJsonInput)
+			errs := packFormJSONValues(context.TODO(), schemaData, testFormJsonSchemaKey, tcase.input)
 			for _, err2 := range errs {
 				t.Errorf("error bubbled from packFormJSONValues: %v", err2)
 			}
